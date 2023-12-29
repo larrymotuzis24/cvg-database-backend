3f014fa0e6e26476d336093d795b5ec1c71d13f4
Larry Motuzis <mlauris2006@yahoo.com>
2023-12-29 15:03:45 -0600

    update

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'larrymotuzis',
-    host: 'localhost',
-    database: 'cvg_database',
-    password: '',
-    port: 5432,
+    user: 'cvg-db',
+    host: 'app-1e7b33d3-1709-42f2-8d0f-743b41672cc5-do-user-15450681-0.c.db.ondigitalocean.com',
+    database: 'cvg-db',
+    password: 'AVNS_ElzadMYlrK0mSRgJHxh',
+    port: 25060,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
